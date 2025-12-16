6ebb375a1686812ea99e2f5c58f87f461aac0aa7
harsh21082004 <harshtiwari.up2108@gmail.com>
2025-12-16 11:42:28 +0530

    Added deployment scripts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // TIWARI JI: Use the full URL here for local testing
-  apiUrl: 'https://ultron-eight.vercel.app/api', 
-  fastApiUrl: 'https://ultron-eight.vercel.app/api/py' 
+  apiUrl: '/api', 
+  fastApiUrl: '/api/py' 
 };
