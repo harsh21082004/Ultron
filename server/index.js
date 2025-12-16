ea32a411959dbfeac45544622936eba223455169
harsh21082004 <harshtiwari.up2108@gmail.com>
2025-12-16 12:32:05 +0530

    Added deployment scripts

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+dotenv.config();
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require('./routes/chat.routes');
 const { notFound, errorHandler } = require('./middlewares/error.middleware');
 const passport = require('passport');
 
-dotenv.config();
 
 connectDB();
 
